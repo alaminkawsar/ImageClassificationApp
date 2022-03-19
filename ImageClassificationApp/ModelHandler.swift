61f73ab5849e9db0ce2951cdc8ff78d07588153d
alaminkawsar <alaminkawsar8643@gmail.com>
2022-03-19 15:08:53 +0600

    - ModelParser class added

---
+++ ImageClassificationApp/ModelHandler.swift
@@ -0,0 +1,13 @@
+//
+//  ModelHandler.swift
+//  ImageClassificationApp
+//
+//  Created by Khayrul on 3/19/22.
+//
+
+import Foundation
+import TensorFlowLite
+
+class ModelHandler {
+    
+}
---
+++ ImageClassificationApp/ModelParser.swift
@@ -0,0 +1,14 @@
+//
+//  ModelParser.swift
+//  ImageClassificationApp
+//
+//  Created by Khayrul on 3/19/22.
+//
+
+import Foundation
+
+import TensorFlowLite
+
+class ModelParser {
+    
+}
