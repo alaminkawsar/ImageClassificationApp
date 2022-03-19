72890695a7b96982e8238924e5b4396f5db08b16
alaminkawsar <alaminkawsar8643@gmail.com>
2022-03-19 15:01:16 +0600

    -user interface added

--- ImageClassificationApp/ViewController.swift
+++ ImageClassificationApp/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var image: UIImageView!
+    @IBAction func pressButton(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
